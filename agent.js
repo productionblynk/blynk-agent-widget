ad9307d68d0c1dda028c87cf6cc5b909f66b1c54
productionblynk <142355126+productionblynk@users.noreply.github.com>
2026-02-05 19:32:32 -0500

    Update agent.js

--- agent.js
+++ agent.js
@@ -346,7 +346,7 @@
 
 #${ROOT_ID} .blynk-subcopy{
   padding: 0 16px 10px;
-  color: rgba(249,21,38,.60);
+  color: rgba(80,77,97,.60);
   font-size:14px;
   line-height:1.45;
 }
@@ -407,7 +407,7 @@
   line-height:1.45;
   position:relative;
   overflow:hidden;
-  color: rgba(249,21,38,100);
+  color: rgba(80,77,97,100);
 }
 #${ROOT_ID} .blynk-bubble:before{
   content:"";
